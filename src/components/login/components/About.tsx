6c2a6ba9803262ee85f70c887a64a260e9e7b84a
virginiarosed <virginiarosedichoso@gmail.com>
2026-01-06 21:23:32 +0800

    update: home pic

--- src/components/login/components/About.tsx
+++ src/components/login/components/About.tsx
@@ -118,7 +118,7 @@ export function About() {
             transition={{ duration: 0.6 }}
           >
             <ImageWithFallback
-              src="https://images.unsplash.com/photo-1748256622734-92241ae7b43f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHx0ZWFtJTIwY29sbGFib3JhdGlvbiUyMHdvcmtzcGFjZXxlbnwxfHx8fDE3NTk5MDI4ODV8MA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral"
+              src="user_home.png"
               alt="Team collaboration"
               className="w-full rounded-2xl shadow-2xl"
             />
